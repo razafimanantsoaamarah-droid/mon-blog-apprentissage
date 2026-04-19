fb44ecf22fe3f3f992a7d9b553bfe1c2f76a73e6
razafimanantsoaamarah-droid <razafimanantsoaamarah@gmail.com>
2026-04-19 08:28:12 +0000

    ci: ajout du workflow GitHub Actions pour le build auto Front et Back

---
+++ backend/src/test/java/com/mekill404/depense_tracert/DepenseTracertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mekill404.depense_tracert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepenseTracertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
