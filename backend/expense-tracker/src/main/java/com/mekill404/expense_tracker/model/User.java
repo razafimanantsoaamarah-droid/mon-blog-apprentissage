70c8aa8e79bafec0b824c6a05cfe9b20fb87fba6
razafimanantsoaamarah-droid <rarafimanantsoaamarah@gmail.com>
2026-04-21 09:06:52 +0300

    refactor(User): ajout du type enum pour la gestion des roles

--- backend/expense-tracker/src/main/java/com/mekill404/expense_tracker/model/User.java
+++ backend/expense-tracker/src/main/java/com/mekill404/expense_tracker/model/User.java
@@ -1,5 +1,7 @@
 package com.mekill404.expense_tracker.model;
 
+import javax.management.relation.Role;
+
 import com.mekill404.expense_tracker.model.enumerate.Genre;
 
 import jakarta.persistence.Column;
@@ -20,29 +22,42 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 public class User {
+	/**
+	 * @Id: qui indique que c'est la clé primaire de la table
+	 * @GeneratedValue: qui indique que la valeur de cette colonne sera générée automatiquement par la base de données
+	 * @Column: qui permet de définir les propriétés de la colonne dans la base de données (unique, nullable, columnDefinition)
+	 * @Enumerated: qui indique que cette colonne est de type énumération et qui permet de spécifier comment les valeurs de l'
+	 * énumération seront stockées dans la base de donnée (EnumType.STRING pour stocker les
+	 * valeurs de l'énumération sous forme de chaînes de caractères, EnumType.ORDINAL pour stocker les valeurs de
+	 *  l'énumération sous forme d'entiers correspondant à leur position dans l'énumération)
+	 */
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(unique = true, nullable = false)
+	@Column(unique = true, nullable = false, columnDefinition = "varchar(255)")
 	private String email;
 
-	@Column(nullable = false)
-    private String role;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, columnDefinition = "varchar(255) default 'ROLE_USER'")
+    private Role role;
 
-	@Column(name="telephone", nullable = false)
+	@Column(name="telephone", nullable = false , columnDefinition = "varchar(255) default '00000000'")
 	private String telephone;
 
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "varchar(255)")
 	private String password;
 
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "varchar(255)")
 	private String last_name;
 
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "varchar(255)")
 	private String first_name;
 
 	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "varchar(255) default 'HOMME'")
 	private Genre genre;
+
+	@Column(nullable = false, columnDefinition = "varchar(255) default 'https://www.w3schools.com/howto/img_avatar.png'")
+	private String url_profil;
 }
---
+++ backend/expense-tracker/src/main/java/com/mekill404/expense_tracker/model/enumerate/Role.java
@@ -0,0 +1,6 @@
+package com.mekill404.expense_tracker.model.enumerate;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
